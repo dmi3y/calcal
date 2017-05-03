c87d82a7ca2b01b7ac9541d976c8b3d1ed8d0dde
dmi3y <lapshukov@gmail.com>
2017-05-02 22:34:20 -0700

    playing around with grids

--- src/store/appReducers.js
+++ src/store/appReducers.js
@@ -1,9 +1,7 @@
 import dataSheetReducer from './dataSheetReducer'
-import layoutReducer from './layoutReducer'
 import userDataReducer from './userDataReducer'
 
 export default {
   dataSheet: dataSheetReducer,
-  layout: layoutReducer,
   user: userDataReducer
 }
